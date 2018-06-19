61842e81dc9165e65ef996ec6ab42c0f96519463
Taylor Bailey <taylor.williams.bailey@gmail.com>
2018-06-19 13:26:14 -0500

    info modal added

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Heading from './Heading.js'
 import BottomNav from './BottomNav.js'
 import MapSelect from './MapSelect.js'
+import InfoModal from './InfoModal.js'
 
 const CarryMap = () => {
     return (
         <div>
             <Heading title="Map" />
             <MapSelect />
+            <InfoModal />
             <BottomNav />
         </div>
     )
