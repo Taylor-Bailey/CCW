ae9f86be8d42ffefcf84ea5765daaca11aa8d0d8
Taylor Bailey <taylor.williams.bailey@gmail.com>
2018-06-20 16:07:03 -0500

    added getMapColor funcion--getting data in console

--- src/components/CopyMap.js
+++ src/components/CopyMap.js
@@ -1,33 +1,64 @@
 import React, { Component } from 'react'
 import USAMap from "react-usa-map"
 import './InteractiveMap.css'
+import { GetAllStates } from './data.js'
 
+class TNMap extends Component {   
 
-class TNMap extends Component {
+    getMapColor = () => {
+        let selectedState = this.props.selectedState;
 
-    tnReciprocityRender = (abbreviation) => {
-        {abbreviation: {
-            fill: "rgb(89, 77, 255)"}
-        }
-
-        return {
-            "NJ": {
-              fill: "navy",
-              clickHandler: (event) => console.log('Custom handler for NJ', event.target.dataset)
-            },
-            "NY": {
-              fill: "#CC0000"
-            }
-          };
-        };
-   
-    render() {
-      return (
-        <div className="usaMap">
-          <USAMap customize={this.tnReciprocityRender()}/>
-        </div>
-      );
+        GetAllStates()
+            .then((resp) => {
+                let reciprocity = resp[selectedState].reciprocity;
+                let noReciprocity = resp[selectedState].no_reciprocity;
+                return [reciprocity, noReciprocity];
+            }).then((arrays) =>{
+                let longerArray = arrays[0].length > arrays[1].length ? arrays[0] : arrays[1]
+                let testObj = {};
+                for(let i = 0; i < longerArray.length; i++) {
+                    testObj[arrays[0][i]] = {fill: "rgb(89, 77, 255)"};
+                    testObj[arrays[1][i]] = {fill: "red"};
+                }
+                return testObj
+            })
     }
-  }
-   
+    render() {        
+        return (
+            <div className="usaMap">
+                <USAMap customize = {this.getMapColor()}/>
+            </div>
+        );
+    }
+};
+        
   export default TNMap
+
+                    ///////Code Before Refactoring///////
+
+      //     let FindReciprocity = (array, selectedState) => {
+    //         array.forEach(stateObject => { 
+    //             for (let key in stateObject) {
+    //                 if (key === "abbreviation") {
+    //                     if (stateObject[key] === selectedState) {
+    //                         console.log("state reciprocity: ", stateObject.reciprocity);
+    //                         return stateObject.reciprocity;
+    //                     }
+    //                         // console.log("reciprocity: ", stateObject.reciprocity);
+    //                 }
+    //             }
+    //         });
+    //     }
+
+    //     let FindNoReciprocity = (array, selectedState) => {
+    //        array.forEach(stateObject => {
+    //            for (let key in stateObject) {
+    //                if (key === "abbreviation") {
+    //                    if (stateObject[key] === selectedState) {
+    //                        console.log("state non reciprocity: ", stateObject.no_reciprocity);
+    //                        return stateObject.no_reciprocity;
+    //                    }
+    //                }
+    //            }
+    //        });
+    //    }  
