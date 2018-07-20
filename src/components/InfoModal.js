775d4effbd837226cca70e9620e1df39a99b30b0
Taylor Bailey <taylor.williams.bailey@gmail.com>
2018-07-19 20:08:53 -0600

    completed state reciprocity JSON and added loader to map render

--- src/components/InfoModal.js
+++ src/components/InfoModal.js
@@ -7,7 +7,7 @@ const InfoModal = () => (
     <Modal.Content className="infoModal">
       <Modal.Description  className="modalDescription">
         <Header><h2>Reciprocity Map</h2></Header>
-        <p>The <span className="blueText">Reciprocity Map</span> shows all states that <span className="blueText">honor</span> your CCW Permit.</p>
+        <p>The <span className="blueText">Reciprocity Map</span> shows all states that <span className="blueText">honor</span> , <span className="redText">do not honor</span>, or only honor your <span className="yellowText">residential</span> concealed weapons permit.  It also shows any states that require an <span className="orangeText">enhanced permit </span> or have <span className="orangeText">extra stipulations</span>.</p>
       </Modal.Description>
     </Modal.Content>
   </Modal>
